8d51f7401cf918af48c47184ecd92993f20650b9
siba98 <gyansiba.patro@gmail.com>
2021-09-27 22:55:08 +0530

    Siba:[Added] Checking Maximum String  Number

---
+++ UC3_MaxStringNumCheck.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day13_Generics_Assignments
+{
+    class UC3_MaxStringNumCheck
+    {
+        public static string MaximumStringNumber(string firstString, string secondString, string thirdString)
+        {
+            if(firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) > 0 ||
+               firstString.CompareTo(secondString) >= 0 && firstString.CompareTo(thirdString) > 0 ||
+               firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) >= 0)
+            {
+                return firstString;
+            }
+            if(secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) > 0 ||
+               secondString.CompareTo(firstString) >= 0 && secondString.CompareTo(thirdString) > 0 ||
+               secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) >= 0)
+            {
+                return secondString;
+            }
+            if(thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) > 0 ||
+               thirdString.CompareTo(firstString) >= 0 && thirdString.CompareTo(secondString) > 0 ||
+               thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) >= 0)
+            {
+                return thirdString;
+            }
+            throw new Exception("FirstNumber, SecondNumber and ThirdNumber are same or Maybe 2 Numbers are same");
+        }
+    }
+}
