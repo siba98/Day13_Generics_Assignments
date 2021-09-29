72e21915845b155fcd7e69cd7e4ef3be56dbbe0f
siba98 <gyansiba.patro@gmail.com>
2021-09-29 17:05:14 +0530

    Siba:[Added] Refactoring all the 3 methods in a Class and Finding max value

---
+++ Refactor2_Generic_Class.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day13_Generics_Assignments
+{
+    public class Refactor2_Generic_Class<T> where T : IComparable
+    {
+        public T[] value;
+        public Refactor2_Generic_Class(T[] value)
+        {
+            this.value = value;
+        }
+
+        public static int MaximumIntNumber(int firstNum, int secondNum, int thirdNum)
+        {
+            if (firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) > 0)
+            {
+                return firstNum;
+            }
+            if (secondNum.CompareTo(firstNum) > 0 && secondNum.CompareTo(thirdNum) > 0)
+            {
+                return secondNum;
+            }
+            if (thirdNum.CompareTo(firstNum) > 0 && thirdNum.CompareTo(secondNum) > 0)
+            {
+                return thirdNum;
+            }
+            throw new Exception("FirstNumber, SecondNumber and ThirdNumber are same or Maybe 2 Numbers are same");
+        }
+
+        public static float MaxFloatNumber(float firstNum, float secondNum, float thirdNum)
+        {
+            if (firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) > 0)
+            {
+                return firstNum;
+            }
+            if (secondNum.CompareTo(thirdNum) > 0 && secondNum.CompareTo(firstNum) > 0)
+            {
+                return secondNum;
+            }
+            if (thirdNum.CompareTo(firstNum) > 0 && thirdNum.CompareTo(secondNum) > 0)
+            {
+                return thirdNum;
+            }
+            throw new Exception("FirstNumber, SecondNumber and ThirdNumber are same or Maybe 2 Numbers are same");
+        }
+
+        public static string MaximumStringNumber(string firstString, string secondString, string thirdString)
+        {
+            if (firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) > 0 ||
+               firstString.CompareTo(secondString) >= 0 && firstString.CompareTo(thirdString) > 0 ||
+               firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) >= 0)
+            {
+                return firstString;
+            }
+            if (secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) > 0 ||
+               secondString.CompareTo(firstString) >= 0 && secondString.CompareTo(thirdString) > 0 ||
+               secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) >= 0)
+            {
+                return secondString;
+            }
+            if (thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) > 0 ||
+               thirdString.CompareTo(firstString) >= 0 && thirdString.CompareTo(secondString) > 0 ||
+               thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) >= 0)
+            {
+                return thirdString;
+            }
+            throw new Exception("FirstNumber, SecondNumber and ThirdNumber are same or Maybe 2 Numbers are same");
+        }
+    }
+}
